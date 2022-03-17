073617a0167c039deaef6fdaa2e04c2486276476
nlatt <nicolas.lattemann@epitech.eu>
2022-03-17 12:19:57 +0100

    proto for ldloader

---
+++ src/graphic/IGraphicsLib.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Description
+*/
+
+class IGraphicsLib {
+    public:
+        virtual ~IGraphicsLib() = default;
+
+        virtual void hello() = 0;
+};
