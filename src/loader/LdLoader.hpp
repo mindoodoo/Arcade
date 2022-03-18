e472c773be885240da591178a731fba235f050c0
Léon Sautour <leon2@sautour.net>
2022-03-18 16:58:17 +0100

    Some repo setup

---
+++ src/loader/LdLoader.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Description
+*/
+
+#pragma once
+
+#include <string>
+#include <dlfcn.h>
+#include <iostream>
+#include "Interface.hpp"
+
+// typedef IGraphicsLib *(*entry)();
+
+template <class T>
+class LDLoader {
+    public:
+        LDLoader() = default;
+        ~LDLoader();
+
+        T *getInstance();
+        void loadLib(std::string libpath);
+
+    private:
+        T *(*_lib_factory)();
+        void *_handle;
+};
