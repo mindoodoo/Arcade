4f7affbad01bb760c3118ed213d2a026fc21cada
Léon Sautour <leon2@sautour.net>
2022-03-21 11:15:54 +0100

    Added rough sketch for IGraphicsLib

--- src/shared_interfaces/IGraphicsLib.hpp
+++ src/shared_interfaces/IGraphicsLib.hpp
@@ -6,15 +6,38 @@
 */
 
 #include <string>
+#include <queue>
+
+// Optionally add tileset width/height in tiles
+typedef struct {
+    std::string asciiTilesetPath;
+    std::string graphicalTilesetPath;
+    std::string fontFolderPath;
+    int tileWidth; // In px
+    int tileHeight; // In px
+    int windowWidth; // In tiles
+    int windowHeight; // In tiles
+} gfx_config_t;
 
 class IGraphicsLib
 {
     public:
         virtual ~IGraphicsLib() = default;
 
-        virtual void display() = 0;
+        // Updates screen with buffer (called at the end of all draw tiles)
+        virtual void display() const = 0;
 
-        virtual void drawTile(int x, int y, int tile) = 0;
+        // Draw tile of index tile_index, at x tile and y tile
+        // (tile is the unit)
         // Note : Could add possibility for rotation of tile in the future
-        // if needed
+        virtual void drawTile(int tile_index, int x, int y) const = 0;
+
+        // Draws text, at x tile and y tile
+        virtual void drawText(const std::string &text, int x, int y) const = 0;
+
+        // Get events that happened since last frame
+        // Queue of events
+        virtual std::queue<char> getInput() const = 0;
+
+        virtual void checkConfig(const gfx_config_t &config) = 0;
 };
