295f4855bd34031000d3cf41bfe4ab4db5a7d324
Léon Sautour <leon2@sautour.net>
2022-03-22 18:17:30 +0100

    Moved loader + implemented basic core

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -9,8 +9,27 @@
 
 Core::Core()
 {
+    // THIS IS HARDCODED AND WILL CHANGE
+    // Load libraries
+    this->_gfxLoader.loadLib("./lib/ncurseslib.so");
+    this->_gameLoader.loadLib("./lib/arcade_nibbler.so");
+
+    // Get instances of game and gfx libraries
+    this->_gfxPtr = this->_gfxLoader.getInstance();
+    this->_gamePtr = this->_gameLoader.getInstance();
 }
 
 Core::~Core()
 {
+    if (this->_gamePtr)
+        delete this->_gamePtr;
+    if (this->_gfxPtr)
+        delete this->_gfxPtr;
+}
+
+void Core::mainLoop(void)
+{
+    while(true) {
+        this->_gamePtr->frame();
+    }
 }
--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -7,11 +7,21 @@
 
 #pragma once
 
+#include "IGraphicsLib.hpp"
+#include "IGameLib.hpp"
+#include "./loader/LdLoader.hpp"
+
 class Core {
     public:
         Core();
         ~Core();
 
+        // Main loop
+        void mainLoop(void);
+
     private:
-        
+        IGraphicsLib *_gfxPtr;
+        IGameLib *_gamePtr;
+        LDLoader<IGraphicsLib> _gfxLoader;
+        LDLoader<IGameLib> _gameLoader;
 };
