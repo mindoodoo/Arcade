2f8b7960e868cca56550c2739f1718e2f2139792
Léon Sautour <leon2@sautour.net>
2022-03-22 17:39:56 +0100

    Added Core class

---
+++ src/core/Core.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Core.cpp
+** File description:
+** .
+*/
+
+#include "Core.hpp"
+
+Core::Core()
+{
+}
+
+Core::~Core()
+{
+}
---
+++ src/core/Core.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Core.hpp
+** File description:
+** .
+*/
+
+#pragma once
+
+class Core {
+    public:
+        Core();
+        ~Core();
+
+    private:
+        
+};
