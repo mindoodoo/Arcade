5cc0d419b64bcf139a80cd572717f16303cf5bf0
Léon Sautour <leon2@sautour.net>
2022-03-22 18:01:15 +0100

    Added rough IGameLib

---
+++ src/shared/IGameLib.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** IGameLib.hpp
+** File description:
+** .
+*/
+
+#pragma once
+
+#include "IGraphicsLib.hpp"
+
+class IGameLib {
+    public:
+        ~IGameLib() = default;
+
+        virtual void frame() = 0;
+        virtual void setGfx(IGraphicsLib **_gfx) = 0;
+
+    protected:
+        IGraphicsLib **_gfx;
+};
