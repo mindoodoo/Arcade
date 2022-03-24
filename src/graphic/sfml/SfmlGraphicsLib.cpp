1711fe79406005b72ea21b33f0d3ffb6d0b7f783
RaizorZawarudo <mickael.riess@epitech.eu>
2022-03-24 15:51:08 +0100

    init window + display window, next , draw functions

--- src/graphic/sfml/SfmlGraphicsLib.cpp
+++ src/graphic/sfml/SfmlGraphicsLib.cpp
@@ -6,7 +6,6 @@
 */
 
 #include "SfmlGraphicsLib.hpp"
-#include "shared.hpp"
 
 SfmlGraphicsLib::SfmlGraphicsLib()
 {
@@ -19,6 +18,37 @@ SfmlGraphicsLib::SfmlGraphicsLib()
         0,
         0
     };
+    this->window.create(sf::VideoMode(640,480), "Arcade SFML");
+    this->window.setFramerateLimit(60);
+}
+
+SfmlGraphicsLib::~SfmlGraphicsLib()
+{
+    window.close();
+}
+
+void SfmlGraphicsLib::createWindow(int width, int height)
+{
+    sf::CircleShape shape(200);
+
+    while (window.isOpen()) 
+    {
+        sf::Event event;
+        while (
+            window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+  
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+}
+
+void loadConfig(void)
+{
+
 }
 
 void SfmlGraphicsLib::drawTile(int tile_index, int x, int y) const
@@ -26,3 +56,30 @@ void SfmlGraphicsLib::drawTile(int tile_index, int x, int y) const
 
 
 }
+
+void SfmlGraphicsLib::display() const
+{
+
+}
+
+void SfmlGraphicsLib::flush() const
+{
+
+}
+
+void SfmlGraphicsLib::drawText(const std::string &text, int x, int y) const
+{
+
+}
+
+std::queue<char> & SfmlGraphicsLib::getInput()
+{
+    
+
+}   
+
+void SfmlGraphicsLib::checkConfig(const gfx_config_t &config)
+{
+
+}
+
--- src/graphic/sfml/SfmlGraphicsLib.hpp
+++ src/graphic/sfml/SfmlGraphicsLib.hpp
@@ -7,27 +7,50 @@
 
 #pragma once
 
-#include "IGraphicsLib.hpp"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "../../shared/IGraphicsLib.hpp"
 
 class SfmlGraphicsLib : public virtual IGraphicsLib
 {
     public:
-    // Constructor
-    SfmlGraphicsLib();
+        // Constructor
+        SfmlGraphicsLib();
+        ~SfmlGraphicsLib();
 
-    //config / view setup
+        //config / view setup
 
-    void loadConfig(void);
+        void loadConfig(void);
 
+        //create window
+        void createWindow(int width, int height);
 
+        // Updates screen with buffer (called at the end of all draw tiles)
+        virtual void display() const override;
+        // Clear screen/window
 
+        //Runtime methods
+        void flush(void) const override;
+        void drawTile(int tile_index, int x, int y) const override;
+        void drawText(const std::string &text, int x, int y) const override;
 
-    //Runtime methods
-    void drawTile(int tile_index, int x, int y) const override;
+        // Get events that happened since last frame
+        // Queue of events
+        std::queue<char> &getInput() override;
+        
+        // Checks if game config is currently loaded config
+        // If not : calls config loading methods
+        virtual void checkConfig(const gfx_config_t &config) override;
+
 
 
     private:
         gfx_config_t _config;
+        sf::RenderWindow window;
 
 
 
