6a478037579b8662766a0abf2db5b077d6f76c40
nlatt <nicolas.lattemann@epitech.eu>
2022-04-07 16:54:16 +0200

    nibbler corners working

--- src/games/nibbler/src/Player.cpp
+++ src/games/nibbler/src/Player.cpp
@@ -78,9 +78,9 @@ int Nibbler::Player::getTileOrientation(int bodyIndex) const
         if (this->_yMovement != 0)
             return (this->_yMovement == -1 ? ORIENT_BOTTOM : ORIENT_TOP);
     }
-    
+
     // Previous tile has different Y
-    switch(this->_body[bodyIndex - 1].y - this->_body[bodyIndex].y) {
+    switch (this->_body[bodyIndex - 1].y - this->_body[bodyIndex].y) {
         case -1:
             return ORIENT_TOP;
         case 1:
@@ -88,7 +88,7 @@ int Nibbler::Player::getTileOrientation(int bodyIndex) const
     }
 
     // Previous tile has different X
-    switch(this->_body[bodyIndex - 1].x - this->_body[bodyIndex].x) {
+    switch (this->_body[bodyIndex - 1].x - this->_body[bodyIndex].x) {
         case -1:
             return ORIENT_LEFT;
         case 1:
@@ -97,56 +97,33 @@ int Nibbler::Player::getTileOrientation(int bodyIndex) const
     return ORIENT_TOP;
 }
 
-
 int getTurnDirection(Nibbler::segment_t previous, Nibbler::segment_t current, Nibbler::segment_t next)
 {
-    // Left is 0 and right is 1
-    std::cout << "previous (near head) X = " << previous.x << "and current X = " << current.x << " and next X (near tail) = "<< next.x <<std::endl;
-    std::cout << "previous (near head) Y = " << previous.y << "and current Y = " << current.y << " and next Y (near tail) = "<< next.y <<std::endl;
+    int xOffsetPrev = current.x - previous.x;
+    int yOffsetPrev = current.y - previous.y;
+    int xOffsetNext = current.x - next.x;
+    int yOffsetNext = current.y - next.y;
 
+    // 0 -1 -1 0 or -1 0 0 -1 turn left
+    if ((xOffsetNext == 0 && yOffsetNext == -1 && xOffsetPrev == -1 && yOffsetPrev == 0) ||
+        (xOffsetNext == -1 && yOffsetNext == 0 && xOffsetPrev == 0 && yOffsetPrev == -1))
+        return ORIENT_LEFT;
 
-    //take 2
+    // 1 0 0 1 or 0 1 1 0 turn right
+    if ((xOffsetNext == 1 && yOffsetNext == 0 && xOffsetPrev == 0 && yOffsetPrev == 1) ||
+        (xOffsetNext == 0 && yOffsetNext == 1 && xOffsetPrev == 1 && yOffsetPrev == 0))
+        return ORIENT_RIGHT;
 
-    //if going vertically before turn
-    if (previous.y == current.y) {
-        std::cout << "turned to horizontal movement" << std::endl;
-        if (previous.x > current.x) {
-            std::cout << "turned RIGHT" << std::endl;
-        }
-        if (previous.x < current.x) {
-            std::cout << "turned LEFT" << std::endl;
-        }
+    // -1 0 0 -1 or 0 -1 1 0
+    if ((xOffsetNext == 1 && yOffsetNext == 0 && xOffsetPrev == 0 && yOffsetPrev == -1) ||
+        (xOffsetNext == 0 && yOffsetNext == -1 && xOffsetPrev == 1 && yOffsetPrev == 0))
+        return ORIENT_TOP;
 
-    }
-
-    if (previous.x == current.x) {
-        std::cout << "turned vertical movement" << std::endl;
-        if (previous.y > current.y) {
-            std::cout << "turned DOWN" << std::endl;
-        }
-        if (previous.y < current.y) {
-            std::cout << "turned UP" << std::endl;
-        }
-
-    }
-
-    // If going horizontally before turn
-    if (previous.x == current.x) {
-        if (next.y > current.y) {
-            std::cout << "OUIIIIIIIIIIIIIIIIIIIIIIIIII\n";
-            return 1;
-        }
-        else
-            return 0;
-    } else { // If going vertically before turn
-        if (next.x > current.x) {
-            std::cout << "OUIIIIIIIIIIIIIIIIIIIIIIIIII\n";
-            return 1;
-        }
-        else
-            return 0;
-    }
-    return 1;
+    // -1 0 0 1 or 0 1 -1 0 bottom
+    if ((xOffsetNext == -1 && yOffsetNext == 0 && xOffsetPrev == 0 && yOffsetPrev == 1) ||
+        (xOffsetNext == 0 && yOffsetNext == 1 && xOffsetPrev == -1 && yOffsetPrev == 0))
+        return ORIENT_BOTTOM;
+    return 84;
 }
 
 int Nibbler::Player::getCornerOrientation(int bodyIndex) const
@@ -154,26 +131,8 @@ int Nibbler::Player::getCornerOrientation(int bodyIndex) const
     segment_t previous = this->_body[bodyIndex - 1]; // Closest to head
     segment_t current = this->_body[bodyIndex]; // Actual corner piece
     segment_t next = this->_body[bodyIndex + 1]; // Closest to tail
-    int turnDirection = getTurnDirection(previous, current, next);
 
-    if (turnDirection == 0) {
-        if (previous.x < next.x) {
-            std::cout << "TOP\n";
-            if (previous.y > next.y)
-                return (previous.x == current.x ? ORIENT_LEFT : ORIENT_BOTTOM);
-            else
-                return (previous.x == current.x ? ORIENT_RIGHT : ORIENT_TOP);
-        } else { // Bottom corners
-            std::cout << "BOTTOM\n";
-            if (previous.y < next.y)
-                return (previous.x == current.x ? ORIENT_RIGHT : ORIENT_TOP);
-            else
-                return (previous.x == current.x ? ORIENT_LEFT : ORIENT_BOTTOM);
-        }
-    }
-
-    // Top corners
-    return ORIENT_TOP;
+    return getTurnDirection(previous, current, next);
 }
 
 void Nibbler::Player::draw()
@@ -190,7 +149,7 @@ void Nibbler::Player::draw()
         else {
             // Condition to identify corner pieces (works)
             if (this->_body[i - 1].x - this->_body[i + 1].x != 0 &&
-            this->_body[i - 1].y - this->_body[i + 1].y != 0)
+                this->_body[i - 1].y - this->_body[i + 1].y != 0)
                 tile = NIBBLER_CORNER;
             else
                 tile = NIBBLER_BODY1; // This omits alternating tiles
@@ -201,6 +160,9 @@ void Nibbler::Player::draw()
             orientation = this->getCornerOrientation(i);
         else
             orientation = this->getTileOrientation(i);
+
+        if (orientation == 84)
+            tile = NIBBLER_BODY1;
 
         // Draw tile
         GFX->drawTile(tile, this->_body[i].x, this->_body[i].y, orientation);
