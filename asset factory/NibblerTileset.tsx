62ccec98f0974b719f3085292a05bee7b2835ff2
Leon Sautour <leon2@sautour.net>
2022-04-10 22:35:50 +0200

    New map1 for Nibbler

---
+++ asset factory/NibblerTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="NibblerTileset" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="../assets/nibbler/NibblerTileset.png" width="80" height="80"/>
+</tileset>
